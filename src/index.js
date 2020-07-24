bc8cce34ad4380806d0e282487645fe483b329e6
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-07-24 22:26:26 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import 'tachyons';
 
-ReactDOM.render(
-  <div>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>,  
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
