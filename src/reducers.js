58995d6ef3aba2f17c61f33513072c7271fd02af
Andrea Trajkovic <trajkovicandrea@gmail.com>
2020-07-31 23:51:04 +0200

    New actions and reducer update

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,9 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+ } from './constants.js';
 
 const intialState = {
     searchField: ''
@@ -10,5 +15,18 @@ export const searchRobots = (state=intialState, action={}) => {
             return Object.assign({}, state, { searchField: action.payload }); 
         default:
             return state;    
+    }
+}
+
+export const requestRobots = (state=intialState, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true})
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
+        default:
+            return state;            
     }
 }
